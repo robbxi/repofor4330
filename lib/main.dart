cb30ea859fd7175bafdb3167d4efe895ce340460
Joseph Aycock <josephfelixaycock@outlook.com>
2025-02-10 16:50:55 -0600

    joseph

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Rob was here'),
+      home: const MyHomePage(title: 'Rob was here/Joseph was also here'),
     );
   }
 }
